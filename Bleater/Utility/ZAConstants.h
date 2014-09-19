24921e50c26e4f25b24ae14b23371fc58f822f57
Baconthorpe <zabuhoff@gmail.com>
2014-09-18 21:51:38 -0400

    created constants class and updated gitignore

--- Bleater/Utility/ZAConstants.h
+++ Bleater/Utility/ZAConstants.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  ZAConstants.h
 //  Bleater
 //
 //  Created by Ezekiel Abuhoff on 9/18/14.
 //  Copyright (c) 2014 Practice. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
-@interface ViewController : UIViewController
-
+@interface ZAConstants : NSObject
 
 @end
-
---
+++ Bleater/Utility/ZAConstants.m
@@ -0,0 +1,13 @@
+//
+//  ZAConstants.m
+//  Bleater
+//
+//  Created by Ezekiel Abuhoff on 9/18/14.
+//  Copyright (c) 2014 Practice. All rights reserved.
+//
+
+#import "ZAConstants.h"
+
+@implementation ZAConstants
+
+@end
